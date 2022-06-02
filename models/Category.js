7936870927362c6ca04e1ac2270d9be31144ead6
Abyou <emandaabyou@gmail.com>
2022-06-02 06:59:58 -0500

    The database tables' models have been defined

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
+    // Defining id column
+    id: {
+      // These two define the category id as an integer value that cannot be null, otherwise it will be rejected
+      type: DataTypes.INTEGER,
+      allowNull: false,
+
+      // Two of these define the category id as a unique identifier that is automatically generated, incremented, and inserted
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      // These two define the category name as a string value that cannot be null, otherwise it will be rejected
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,62 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    // Defining id column
+    id: {
+      // These one defines the id as an integer value, otherwise it will be rejected
+      type: DataTypes.INTEGER,
+      // This one ensures that the id is not nullable, otherwise it will be rejected
+      allowNull: false,
+
+      // Two of these define the id as a unique identifier that is automatically generated, incremented, and inserted
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    // Defining product name column
+    product_name: {
+      // These two define the product name as a string value that cannot be null, otherwise it will be rejected
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    // Defining price column
+    price: {
+      // These two define the price as a decimal value that cannot be null, otherwise it will be rejected
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // This validation ensures that only decimal values are accepted, otherwise they will be rejected
+      validate: {
+        isDecimal: true,
+      },
+    },
+
+    // Defining stock column
+    stock: {
+      // These two define the stock as an integer value that cannot be null, otherwise it will be rejected
+      type: DataTypes.INTEGER,
+      allowNull: false,
+
+      // This one ensures that if a stock value is not supplied, the default value 20 will automatically inserted
+      defaultValue: 20,
+      // This validation ensures that only numeric values are accepted, otherwise they will be rejected
+      validate: {
+        isNumeric: true,
+      },
+    },
+
+    // Defining category id column
+    category_id: {
+      // These one defines the category id as an integer value, otherwise it will be rejected
+      type: DataTypes.INTEGER,
+
+      // This defines a foreign key that references to the category id primary key (id)
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
