0e06fd91e7ac8058c74b756e568ce5aba7c1dd81
Abyou <emandaabyou@gmail.com>
2022-06-03 03:18:44 -0500

    A route has been established to access the products table

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,40 @@
+// const { Model, DataTypes } = require("sequelize");
+
+// const sequelize = require("../config/connection.js");
+
+// class Category extends Model {}
+
+// Category.init(
+//   {
+//     // define columns
+
+//     // Defining id column
+//     id: {
+//       // These two define the category id as an integer value that cannot be null, otherwise it will be rejected
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+
+//       // Two of these define the category id as a unique identifier that is automatically generated, incremented, and inserted
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     category_name: {
+//       // These two define the category name as a string value that cannot be null, otherwise it will be rejected
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "category",
+//   }
+// );
+
+// module.exports = Category;
+
 const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection.js");
@@ -6,20 +43,13 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-
-    // Defining id column
     id: {
-      // These two define the category id as an integer value that cannot be null, otherwise it will be rejected
       type: DataTypes.INTEGER,
       allowNull: false,
-
-      // Two of these define the category id as a unique identifier that is automatically generated, incremented, and inserted
       primaryKey: true,
       autoIncrement: true,
     },
     category_name: {
-      // These two define the category name as a string value that cannot be null, otherwise it will be rejected
       type: DataTypes.STRING,
       allowNull: false,
     },
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { json } = require("body-parser");
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
@@ -7,12 +8,74 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock"],
+    // Including associated Category and Tag data
+    include: [
+      // Including associated Category data
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+
+      // Including associated Tag data
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((databaseCategoryData) => res.json(databaseCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+
+    attributes: ["id", "product_name", "price", "stock"],
+    // Incorporating Category and Tag data
+    include: [
+      // Incorporating Category data
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+
+      // Incorporating Tag data
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((databaseCategoryData) => {
+      if (!databaseCategoryData) {
+        // Sending a status 400 message to the user if product with the given id is not found
+        res.status(400).json({
+          message: `Sorry, No product with id ${req.params.id} has been found! Please check your input and try again!`,
+        });
+
+        // Sending the product to the user if product with the given id is found
+        return;
+      }
+      // Converting data to json format
+      res.json(databaseCategoryData);
+    })
+    // Sending a status 500 to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -91,6 +154,25 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((databaseCategoryData) => {
+      // Sending a status 404 message to the user if no product with the given id is found
+      if (!databaseCategoryData) {
+        res.status(404).json({
+          message: `Sorry, No product with id ${req.params.id} has been found! Please check your input and try again!`,
+        });
+        return;
+      }
+      res.json(databaseCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
